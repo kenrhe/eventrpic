d4377930839ecaa896e8306aa27d995c781adc2e
Kenneth Rhee <kenneth.h.rhee@gmail.com>
2014-06-26 00:41:17 -0400

    Signed By Kenneth Rhee

---
+++ README.md
@@ -0,0 +1,8 @@
+flask-boilerplate
+=================
+
+Ready to use and deploy to Heroku!
+
+<h3>To run simply do <code>python app.py</code>
+</h3>
+<h3>Once the Heroku remote is setup, simply do <code> git push heroku master</code> to deploy your code</h3>
--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, session, redirect, jsonify
 import jinja2
 import os
 
+import datetime
+import pymongo
+from pymongo import MongoClient
+
 #making a new Flask app
 app = Flask(__name__)
 
@@ -10,7 +14,25 @@ app = Flask(__name__)
 #this function tells the app what to do when it loads the main page
 def eventrpic():
 	#render_template will render the index.html found in the template folder
-	return 'hello world'
+	return render_template('index.html')
+
+@app.route('/find', methods=['GET', 'POST'])
+def find():
+	#returning redirect will cause it do go to the specificed URL
+	if request.method == 'GET':
+		return render_template('find.html')
+	return redirect('/')
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+	if request.method == 'GET':
+		return render_template('create.html')
+	return redirect('/')
+
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
 
 if __name__ == '__main__':
 	#this code starts the web app, it can be found at http://localhost:8000
